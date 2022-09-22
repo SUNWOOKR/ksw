5edcf1aab5a74990750299727002618d8ae04e1b
선우 <111554630+SUNWOOKR@users.noreply.github.com>
2022-09-22 11:24:22 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-let 밝기 = 0
 basic.forever(function () {
-    밝기 = 0
-    if (밝기 >= 20) {
-        basic.showString("day")
-    } else {
-        basic.showString("slight")
-    }
+    basic.showIcon(IconNames.Heart)
 })
